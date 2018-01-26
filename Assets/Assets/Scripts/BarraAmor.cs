32b0c1bcde0b8ef84e487e4aaca5a0ecd8957931
Lin M. Dotor <linmdotor@gmail.com>
2018-01-26 22:15:54 +0100

    First Commit

---
+++ Assets/Assets/Scripts/BarraAmor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraAmor : MonoBehaviour {
+
+	private const float MAX_VALUE = 100.0f;
+
+	public float amorValue = 50.0f;
+	public Image loveBarSlider;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		loveBarSlider.fillAmount = amorValue / MAX_VALUE;
+	}
+}
