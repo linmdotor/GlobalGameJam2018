7b4558682304dcce70ae8cac4e6cb4822f8e3971
Lin M. Dotor <linmdotor@gmail.com>
2018-01-27 14:12:35 +0100

    Added questions manager

--- Assets/Assets/Scripts/QuestionManager.cs
+++ Assets/Assets/Scripts/QuestionManager.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class QuestionManager : MonoBehaviour {
 
+	private XmlParser questionsParser = new XmlParser();
+	private Dictionary<int, XmlParser.Question> questions;
+	public TextAsset XmlAsset;
+
+	private bool[] usedQuestions; // initialized to false
+
 	// Use this for initialization
 	void Start () {
-		
+		questions = questionsParser.XmlToArray(XmlAsset);
+		Debug.Log("Preguntas cargadas correctamente");
+		usedQuestions = new bool[questions.Count];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void LoadNewQuestion()
+	{
+
 	}
 }
