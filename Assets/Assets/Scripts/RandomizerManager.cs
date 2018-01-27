4a2bec026c1243df28cc7f81cbe5e18ab07ad394
Lin M. Dotor <linmdotor@gmail.com>
2018-01-27 13:53:32 +0100

    Created Character Randomizer

---
+++ Assets/Assets/Scripts/RandomizerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class RandomizerManager : MonoBehaviour {
+
+	public List<RandomFeature> randomFeatures;
+	public Text characterName;
+	public List<string> randomNames;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Randomize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Randomize()
+	{
+		Debug.Log("GENERATED NEW CHARACTER: -------------------------");
+		characterName.text = randomNames[Random.Range(0, randomNames.Count)];
+
+		foreach (RandomFeature feature in randomFeatures)
+		{
+			feature.Randomize();
+			Debug.Log(feature.GetFeatureName() + ": " + feature.GetCurrentFeature());
+		}
+	}
+}
