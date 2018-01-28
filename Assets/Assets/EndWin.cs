f89b9a00a59c3747bd30679d9fe9a02859efa434
Lin M. Dotor <linmdotor@gmail.com>
2018-01-28 13:26:38 +0100

    End scenes

--- Assets/Assets/EndWin.cs
+++ Assets/Assets/EndWin.cs
@@ -1,19 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Intro : MonoBehaviour {
+public class EndWin : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update()
 	{
 		if (Input.GetKey(KeyCode.Escape))
 		{
-			SceneManager.LoadScene("MainScene");
+			SceneManager.LoadScene("MainMenu");
 		}
-	}
-
-	public void EndIntro()
-	{
-		SceneManager.LoadScene("MainScene");
 	}
 }
