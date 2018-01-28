850cd47793a6a6cf37a3a38daa4fbda4e06a2fc1
Lin M. Dotor <linmdotor@gmail.com>
2018-01-28 11:59:33 +0100

    Added Intro to BuildSettings

--- Assets/Assets/Scripts/MainMenu.cs
+++ Assets/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour {
 
 	public void PlayButton()
 	{
-		SceneManager.LoadScene("MainScene");
+		SceneManager.LoadScene("Intro");
 	}
 
 	public void CreditsButton()
